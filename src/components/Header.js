8e80909f468b175502701ae71ecce9b7802e579b
m.raghavendra <m.raghavendra@starkdigital.net>
2022-07-05 22:49:53 +0530

    updated code

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ export default function Header() {
     <div className="logo"><a className="navbar-brand" href="/">
         <img src="assets/images/logo.svg" alt="logo"/></a></div>
     <nav className="navbar navbar-expand-lg bg-transparent">
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
